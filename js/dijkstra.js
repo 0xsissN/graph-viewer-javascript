43f09ba79a7f2687f03b0f084a584cc4f1843962
OracleShadow <nico22delcastillo07@gmail.com>
2024-11-07 19:37:42 -0400

    comments

--- js/dijkstra.js
+++ js/dijkstra.js
@@ -1,7 +1,11 @@
+/**
+ * Dijkstra's algorithm
+ * Returns the shortest path as an array
+ */
+
 const dijkstra = (adj, source, destination) => {
-    const inf = Number.MAX_SAFE_INTEGER; // Define un valor infinito utilizando el número máximo en JavaScript.
+    const inf = Number.MAX_SAFE_INTEGER; 
     
-    // Inicializa un objeto para almacenar el costo y los predecesores de cada nodo.
     const nodeData = {
         '0': { cost: inf, pred: [] },
         '1': { cost: inf, pred: [] },
@@ -14,36 +18,34 @@ const dijkstra = (adj, source, destination) => {
         '8': { cost: inf, pred: [] }
     };
 
-    nodeData[source].cost = 0; // Establece el costo del nodo de origen a 0.
+    nodeData[source].cost = 0; 
 
-    let visited = []; // Inicializa un array para rastrear los nodos visitados.
-    let temp = source; // Variable temporal que guarda el nodo actual.
-    let minHeap = []; // Inicializa un array para almacenar los nodos vecinos que se explorarán.
+    let visited = []; 
+    let temp = source; 
+    let minHeap = []; 
 
-    for (let i = 0; i < 7; i++) { // Realiza un bucle para visitar un número fijo de nodos.
-        if (!visited.includes(temp)) { // Verifica si el nodo actual no ha sido visitado.
-            visited.push(temp); // Marca el nodo actual como visitado.
+    for (let i = 0; i < 7; i++) { 
+        if (!visited.includes(temp)) { 
+            visited.push(temp); 
             
-            for (let neighbor in adj[temp]) { // Itera sobre los vecinos del nodo actual.
-                if (!visited.includes(neighbor)) { // Verifica si el vecino no ha sido visitado.
-                    let cost = nodeData[temp].cost + adj[temp][neighbor]; // Calcula el costo total hasta el vecino.
+            for (let neighbor in adj[temp]) { 
+                if (!visited.includes(neighbor)) {
+                    let cost = nodeData[temp].cost + adj[temp][neighbor]; 
 
-                    // Si el nuevo costo es menor que el costo registrado, actualiza el costo y el predecesor.
                     if (cost < nodeData[neighbor].cost) {
-                        nodeData[neighbor].cost = cost; // Actualiza el costo del vecino.
-                        nodeData[neighbor].pred = [...nodeData[temp].pred, temp]; // Establece el predecesor del vecino.
+                        nodeData[neighbor].cost = cost; 
+                        nodeData[neighbor].pred = [...nodeData[temp].pred, temp];
                     }
-                    minHeap.push(neighbor) // Agrega el vecino a la lista de nodos para explorar.
+                    minHeap.push(neighbor)
                 }
             }
 
-            minHeap.sort() // Ordena el minHeap para encontrar el siguiente nodo con el menor costo.
-            temp = minHeap[0] // Selecciona el nodo con el costo más bajo como el próximo nodo a visitar.
-            minHeap.shift() // Elimina el nodo con el costo más bajo del minHeap.
+            minHeap.sort()
+            temp = minHeap[0] 
+            minHeap.shift() 
         }
     }
 
-    // Crea el camino a partir de los predecesores del nodo de destino.
     let path = [...nodeData[destination].pred, destination] 
-    pathAlg(path) // Llama a la función pathAlg para procesar el camino encontrado.
+    pathAlg(path) 
 }
--- js/index.js
+++ js/index.js
@@ -1,124 +1,158 @@
-const canvas = $('#map').get(0) // Selecciona el elemento <canvas> con id "map" usando jQuery y obtiene el primer elemento (el DOM).
-const ctx = canvas.getContext('2d') // Obtiene el contexto 2D del canvas para poder dibujar en él.
+/**
+ * Create the map using canva 
+ */
 
-$('.toggle-btn').on('click', () => {
-    $('#sidebar').toggleClass('expand') // Añade o quita la clase "expand" del sidebar cuando se haga clic en el botón de alternancia.
-})
+const canvas = $('#map').get(0)
+const ctx = canvas.getContext('2d')
 
-let adj = [] // Declaración de un array para almacenar la matriz de adyacencia que representa el grafo.
-let source = "" // Variable que almacenará el vértice de origen.
-let destination = "" // Variable que almacenará el vértice de destino.
-let selectedSource = null // Variable que contiene el vértice seleccionado como origen en el mapa.
-let selectedDestination = null // Variable que contiene el vértice seleccionado como destino en el mapa.
-let path = [] // Array para almacenar el camino más corto calculado por el algoritmo de Dijkstra.
+let adj = []
+let source = ""
+let destination = ""
+let selectedSource = null
+let selectedDestination = null
+let path = []
+
+/**
+ * Loads JSON files from an URL
+ */
 
 const loadJSON = async (url) => {
-    const response = await fetch(url) // Función que carga un archivo JSON desde una URL dada.
-    return await response.json() // Retorna el contenido del JSON como objeto.
+    const response = await fetch(url)
+    return await response.json()
 }
 
-const drawMap = async () => {
-    const cities = await loadJSON('../assets/db/cities.json') // Carga la lista de ciudades (vértices) desde un archivo JSON.
-    const routes = await loadJSON('../assets/db/routes.json') // Carga la lista de rutas (aristas) desde un archivo JSON.
+/**
+ * This function draws the map
+ * Loads the data from JSON files
+ * Using coordinates, draws vertices and connections
+ * Creates the graph structure
+ * Handles the logic for displaying the optimized path
+ */
 
-    adj = Array(cities.length).fill(null).map(() => []) // Inicializa la matriz de adyacencia como un array bidimensional vacío.
+const drawMap = async () => {
+    const cities = await loadJSON('../assets/db/cities.json')
+    const routes = await loadJSON('../assets/db/routes.json')
+
+    adj = Array(cities.length).fill(null).map(() => [])
 
     routes.forEach(route => {
-        let start = cities[route.startVertex]; // Obtiene las coordenadas de la ciudad de inicio de la ruta.
-        let end = cities[route.endVertex]; // Obtiene las coordenadas de la ciudad de fin de la ruta.
+        let start = cities[route.startVertex];
+        let end = cities[route.endVertex];
 
-        ctx.beginPath(); // Inicia un nuevo camino en el contexto de dibujo.
-        ctx.moveTo(start.ejeX * canvas.width * 4, start.ejeY * canvas.height); // Mueve el lápiz del canvas al inicio de la ruta.
-        ctx.lineTo(end.ejeX * canvas.width * 4, end.ejeY * canvas.height); // Dibuja una línea desde el punto de inicio hasta el final de la ruta.
+        ctx.beginPath();
+        ctx.moveTo(start.ejeX * canvas.width * 4, start.ejeY * canvas.height);
+        ctx.lineTo(end.ejeX * canvas.width * 4, end.ejeY * canvas.height);
 
-        if(isShortPath(route.startVertex, route.endVertex)){
-            ctx.strokeStyle = 'blue'; // Si la ruta forma parte del camino más corto, pinta la línea de azul.
-        }else{
-            ctx.strokeStyle = 'white'; // Si no es parte del camino más corto, pinta la línea de blanco.
+        if (isShortPath(route.startVertex, route.endVertex)) {
+            ctx.strokeStyle = 'blue';
+        } else {
+            ctx.strokeStyle = 'white';
         }
 
-        ctx.stroke(); // Dibuja la línea en el canvas.
+        ctx.stroke();
 
-        // Calcula el punto medio de la ruta para colocar la distancia en el centro de la línea.
         let mx = ((start.ejeX * canvas.width * 4) + (end.ejeX * canvas.width * 4)) / 2;
         let my = ((start.ejeY * canvas.height) + (end.ejeY * canvas.height)) / 2;
 
-        // Dibuja el texto con la distancia de la ruta en el punto medio.
         ctx.fillStyle = 'white';
         ctx.font = '30px Arial';
         ctx.fillText(`${route.distance}`, mx, my);
 
-        // Añade la distancia entre los vértices en la matriz de adyacencia.
         adj[parseInt(route.startVertex)][parseInt(route.endVertex)] = parseInt(route.distance)
         adj[parseInt(route.endVertex)][parseInt(route.startVertex)] = parseInt(route.distance)
     });
 
     cities.forEach(city => {
-        ctx.beginPath(); // Comienza un nuevo camino para dibujar el vértice.
-        ctx.arc(city.ejeX * canvas.width * 4, city.ejeY * canvas.height, 10, 0, Math.PI * 2) // Dibuja un círculo en la posición de la ciudad.
+        ctx.beginPath();
+        ctx.arc(city.ejeX * canvas.width * 4, city.ejeY * canvas.height, 10, 0, Math.PI * 2)
 
-        // Colorea el vértice dependiendo de si es el origen, destino o ningún punto importante.
-        if(parseInt(city.vertex) == selectedSource){
-            ctx.fillStyle = 'green' // Si es el origen, pinta el vértice de verde.
-        }else if(parseInt(city.vertex) == selectedDestination){
-            ctx.fillStyle = 'red' // Si es el destino, pinta el vértice de rojo.
-        }else{
-            ctx.fillStyle = 'white' // Si no es ni origen ni destino, lo pinta de blanco.
+        if (parseInt(city.vertex) == selectedSource) {
+            ctx.fillStyle = 'green'
+        } else if (parseInt(city.vertex) == selectedDestination) {
+            ctx.fillStyle = 'red'
+        } else {
+            ctx.fillStyle = 'white'
         }
-        
-        ctx.fill() // Rellena el círculo con el color especificado.
+
+        ctx.fill()
     })
 }
 
+/**
+ * This function appends the list of cities to the select element (HTML)
+ */
+
 const loadCities = async () => {
-    const cities = await loadJSON('../assets/db/cities.json') // Carga la lista de ciudades desde el JSON.
-    const sourceVertex = $('#source-vertex') // Obtiene el elemento de selección del vértice de origen.
-    const destinationVertex = $('#destination-vertex') // Obtiene el elemento de selección del vértice de destino.
+    const cities = await loadJSON('../assets/db/cities.json')
+    const sourceVertex = $('#source-vertex')
+    const destinationVertex = $('#destination-vertex')
 
     cities.forEach(city => {
-        // Crea opciones para los selectores de ciudades de origen y destino, basándose en los datos cargados.
         let optionSource = $('<option>').val(city.vertex).text(city.city)
         let destinationSource = $('<option>').val(city.vertex).text(city.city)
-        
-        // Añade las opciones a los selectores de origen y destino.
+
         sourceVertex.append(optionSource)
         destinationVertex.append(destinationSource)
     })
 }
 
+/**
+ * Handles sidebar expand/collapse functionality
+ */
+
+$('.toggle-btn').on('click', () => {
+    $('#sidebar').toggleClass('expand')
+})
+
+/**
+ * Handles the source and destination vertices highlighting
+ */
+
 $('#source-vertex').on('change', (e) => {
-    let eTar = $(e.target).val() // Obtiene el valor seleccionado en el selector de origen.
-    source = eTar // Almacena el vértice de origen seleccionado.
-    selectedSource = parseInt(eTar) // Convierte el valor del origen a número y lo guarda en la variable.
-    drawMap() // Redibuja el mapa con el nuevo origen seleccionado.
+    let eTar = $(e.target).val()
+    source = eTar
+    selectedSource = parseInt(eTar)
+    drawMap()
 })
 
 $('#destination-vertex').on('change', (e) => {
-    let eTar = $(e.target).val() 
-    destination = eTar 
-    selectedDestination = parseInt(eTar) 
-    drawMap() 
+    let eTar = $(e.target).val()
+    destination = eTar
+    selectedDestination = parseInt(eTar)
+    drawMap()
 })
+
+/**
+ * Handles click event to execute Dijkstra's algorithm
+ */
 
 $('#start-alg').on('click', () => {
-    dijkstra(adj, source, destination) // Ejecuta el algoritmo de Dijkstra cuando se hace clic en el botón de "Iniciar algoritmo".
+    dijkstra(adj, source, destination)
 })
 
+
+/**
+ * Draws the shortest path with a different color
+ */
+
 const pathAlg = (pahtA) => {
-    path = pahtA // Almacena el camino más corto resultante del algoritmo.
-    drawMap() // Redibuja el mapa para resaltar el camino más corto.
+    path = pahtA
+    drawMap()
 }
 
+/**
+ * Checks vertices existence in shortest path
+ */
+
 const isShortPath = (startVertex, endVertex) => {
-    for(let i = 0; i < path.length - 1; i++){
-        // Verifica si el tramo entre dos vértices es parte del camino más corto.
-        if((startVertex == path[i] && endVertex == path[i + 1]) || (endVertex == path[i] && startVertex == path[i + 1])){
-            return true // Si es parte del camino más corto, devuelve true.
+    for (let i = 0; i < path.length - 1; i++) {
+        if ((startVertex == path[i] && endVertex == path[i + 1]) || (endVertex == path[i] && startVertex == path[i + 1])) {
+            return true
         }
     }
 
-    return false // Si no es parte del camino más corto, devuelve false.
+    return false
 }
 
-drawMap() // Dibuja el mapa inicialmente.
-loadCities() // Carga las ciudades en los selectores de origen y destino.
+drawMap()
+loadCities() 
